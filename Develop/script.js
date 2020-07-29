8d044fbe7435753f9ce4626236281f7865c8a3e8
Alex <alex@alexjahani.com>
2020-07-29 08:15:48 -0700

    making final changes

---
+++ Develop/script.js
@@ -0,0 +1,26 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// First, list out variables/arrays
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '+', '=', '<', '>', ',', '.', '?']
+
+console.log(upperCase)
+console.log(lowerCase)
+console.log(number)
+console.log(specilal)
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
